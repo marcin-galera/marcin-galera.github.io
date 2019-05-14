e9bcf42205c359a9debcbb2d6505880156d50c6a
marcin-galera <Marcin.galera@outlook.com>
2019-05-14 21:03:20 +0200

    menu app and desktop done

--- main.js
+++ main.js
@@ -42,11 +42,65 @@ button.addEventListener("click", function () {
 
 
 let toogleNav = document.getElementById("myNav");
+let burger = document.querySelector(".burger");
+let barOne = document.querySelector(".bar1");
+let barTwo = document.querySelector(".bar2");
+let barThree = document.querySelector(".bar3");
+let hrefElements = document.querySelectorAll('.overlay-content > *')
+let menuBar = document.querySelector(".overlay-content")
 
-function openNav() {
-    if (toogleNav.style.width = "0") {
+if (window.innerWidth > 600) {
+    toogleNav.style.width = '100%';
+} else {
+    toogleNav.style.width = '0px';
+}
+
+function showMenu() {
+    if (toogleNav.style.width == "0px") {
         toogleNav.style.width = "100%";
-    } else if (toogleNav.style.width = "100%") {
-        toogleNav.style.width = "0"
+        barOne.classList.add('change1');
+        barTwo.classList.add('change2');
+        barThree.classList.add('change3');
+
+    } else {
+        toogleNav.style.width = "0px";
+        barOne.classList.remove('change1');
+        barTwo.classList.remove('change2');
+        barThree.classList.remove('change3');
     }
 }
+
+if (window.innerWidth < 600) {
+    function hideMenu() {
+        toogleNav.style.width = "0px";
+        barOne.classList.remove('change1');
+        barTwo.classList.remove('change2');
+        barThree.classList.remove('change3');
+    }
+}
+
+
+window.onscroll = function () {
+    myFunction()
+};
+
+function myFunction() {
+    if (document.body.scrollTop > 473 || document.documentElement.scrollTop > 473) {
+        
+            hrefElements[0].style.padding = "6px 12px 6px 12px";
+            hrefElements[1].style.padding = "6px 12px 6px 12px";
+            hrefElements[2].style.padding = "6px 12px 6px 12px";
+            hrefElements[3].style.padding = "6px 12px 6px 12px";
+            hrefElements[4].style.padding = "6px 12px 6px 12px";
+        menuBar.style.backgroundColor = "black";
+        menuBar.style.height = "30px";
+        } else {
+            hrefElements[0].style.padding = "7px 20px 7px 20px";
+            hrefElements[1].style.padding = "7px 20px 7px 20px";
+            hrefElements[2].style.padding = "7px 20px 7px 20px";
+            hrefElements[3].style.padding = "7px 20px 7px 20px";
+            hrefElements[4].style.padding = "7px 20px 7px 20px";
+        menuBar.style.backgroundColor = "black";
+        menuBar.style.height = "40px";
+        }
+    }
